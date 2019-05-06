83d6ddc474cdcd986e64f351ab2b888ebbe3467b
Arjun Attam <arjunattam@gmail.com>
2019-05-06 12:02:57 -0700

    Add elixir/mix boilerplate

---
+++ lib/elixir_lsif.ex
@@ -0,0 +1,18 @@
+defmodule ElixirLSIF do
+  @moduledoc """
+  Documentation for ElixirLSIF.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirLSIF.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_lsif_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirLSIFTest do
+  use ExUnit.Case
+  doctest ElixirLSIF
+
+  test "greets the world" do
+    assert ElixirLSIF.hello() == :world
+  end
+end
